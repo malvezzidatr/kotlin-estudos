70b2ca06e5040a850a62608b072ee23f212f9ff2
Caio Malvezzi <malvezzi.dev@gmail.com>
2026-07-10 13:59:31 -0300

    first commit

---
+++ HelloKMP/app/shared/src/commonMain/kotlin/com/kmp/Greeting.kt
@@ -0,0 +1,9 @@
+package com.kmp
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return sayHello(platform.name)
+    }
+}
---
+++ HelloKMP/core/src/commonMain/kotlin/com/kmp/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.kmp
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
