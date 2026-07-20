9326428e959d5ee05164a18bdf10fa2f6d3593d0
Caio Malvezzi <malvezzi.dev@gmail.com>
2026-07-20 12:05:01 -0300

    understading more about buttons

--- LearningXML/app/src/main/java/com/estudo/learningxml/MainActivity.kt
+++ LearningXML/app/src/main/java/com/estudo/learningxml/MainActivity.kt
@@ -2,25 +2,43 @@ package com.estudo.learningxml
 
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var myText: TextView
+    lateinit var myButton1: Button
+    lateinit var myButton2: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         myText = findViewById(R.id.textExample)
+        myButton1 = findViewById(R.id.buttonExample1)
+        myButton2 = findViewById(R.id.buttonExample2)
         myText.setTextColor(Color.YELLOW)
         myText.setText("This is an example")
         myText.setOnClickListener {
             myText.setTextColor(Color.GREEN)
+        }
+
+        myButton1.setOnClickListener {
+            myButton1.setBackgroundColor(Color.BLACK)
+            myText.setText("Button 1 has clicked")
+            myText.isVisible = false
+            myButton1.isVisible = false
+        }
+
+        myButton2.setOnClickListener {
+            myText.isVisible = true
+            myButton1.isVisible = true
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
