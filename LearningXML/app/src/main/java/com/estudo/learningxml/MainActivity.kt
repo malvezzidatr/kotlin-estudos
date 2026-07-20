61a9bec2ea0ad2b679d827250e45c8b12a962306
Caio Malvezzi <malvezzi.dev@gmail.com>
2026-07-20 11:49:54 -0300

    Learning about TextView

--- LearningXML/app/src/main/java/com/estudo/learningxml/MainActivity.kt
+++ LearningXML/app/src/main/java/com/estudo/learningxml/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.estudo.learningxml
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var myText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        myText = findViewById(R.id.textExample)
+        myText.setTextColor(Color.YELLOW)
+        myText.setText("This is an example")
+        myText.setOnClickListener {
+            myText.setTextColor(Color.GREEN)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
